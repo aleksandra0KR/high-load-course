a0643005af4df05334450780a13ce7d1cbd5b2b0
aleksandraKriuchkova <kryuchkova.a23@rwb.ru>
2025-10-24 00:01:04 +0300

    feat: 2 works meow meow

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.orders.repository.OrderRepository
-import ru.quipy.payments.logic.OrderPayer
 import ru.quipy.payments.metrics.PaymentMetrics
 import java.util.*
-import java.time.Duration
 
 @RestController
 class APIController {
@@ -22,7 +19,10 @@ class APIController {
     private lateinit var orderRepository: OrderRepository
 
     @Autowired
-    private lateinit var orderPayer: OrderPayer
+    private lateinit var paymentQueueProcessor: PaymentQueueProcessor
+
+    @Autowired
+    private lateinit var paymentMetrics: PaymentMetrics
 
     @PostMapping("/users")
     fun createUser(@RequestBody req: CreateUserRequest): User {
@@ -30,7 +30,6 @@ class APIController {
     }
 
     data class CreateUserRequest(val name: String, val password: String)
-
     data class User(val id: UUID, val name: String)
 
     @PostMapping("/orders")
@@ -59,32 +58,21 @@ class APIController {
         PAID,
     }
 
-    private val slidingWindow = 1
-    private val rateLimit = 11
-    private val rateLimiter = SlidingWindowRateLimiter(rateLimit.toLong(), Duration.ofSeconds(slidingWindow.toLong()))
-
-    @Autowired
-    private lateinit var paymentMetrics: PaymentMetrics
-
     @PostMapping("/orders/{orderId}/payment")
     fun payOrder(@PathVariable orderId: UUID, @RequestParam deadline: Long): ResponseEntity<PaymentSubmissionDto> {
         paymentMetrics.markIncomingRequest()
-        val paymentId = UUID.randomUUID()
-
-        if (!rateLimiter.tick()) {
-            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build()
-        }
-
 
         val order = orderRepository.findById(orderId)?.let {
             orderRepository.save(it.copy(status = OrderStatus.PAYMENT_IN_PROGRESS))
             it
-        } ?: throw IllegalArgumentException("No such order $orderId")
+        } ?: return ResponseEntity.status(HttpStatus.NOT_FOUND).build()
 
+        val paymentId = UUID.randomUUID()
 
-        val createdAt = orderPayer.processPayment(orderId, order.price, paymentId, deadline)
+        paymentQueueProcessor.submitPaymentTask(orderId, order.price, paymentId, deadline)
         paymentMetrics.markSuccessfulRequest()
-        return ResponseEntity.ok(PaymentSubmissionDto(createdAt, paymentId))
+
+        return ResponseEntity.ok(PaymentSubmissionDto(System.currentTimeMillis(), paymentId))
     }
 
     class PaymentSubmissionDto(
