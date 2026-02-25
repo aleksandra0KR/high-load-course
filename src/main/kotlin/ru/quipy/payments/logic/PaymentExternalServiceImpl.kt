e7a5be823593422b934fb99e226da5a631c7e684
kryuchkova.a23 <kryuchkova.a23@rwb.ru>
2026-02-26 01:03:30 +0300

    fix: db request into async

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -92,7 +92,7 @@ class PaymentExternalSystemAdapterImpl(
                 )
             )
             .POST(HttpRequest.BodyPublishers.noBody())
-            .timeout(Duration.ofSeconds(5))
+            .timeout(Duration.ofSeconds(3))
             .build()
 
         val start = System.currentTimeMillis()
