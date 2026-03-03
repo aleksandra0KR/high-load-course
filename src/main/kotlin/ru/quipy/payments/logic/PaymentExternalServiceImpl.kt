ddb167dd937a6e51ba204be44908d2ca9c18a5cd
Andrey <andrey.a-petrovskiy@yandex.ru>
2026-03-04 00:30:58 +0300

    fix

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -49,12 +49,12 @@ class PaymentExternalSystemAdapterImpl(
 
     private val rateLimiter = SlidingWindowRateLimiter(
         properties.rateLimitPerSec.toLong(),
-        Duration.ofSeconds(2)
+        Duration.ofMillis(1500L)
     )
 
 
     private val maxRetries = 2
-    private val retryDelayMs = 300L
+    private val retryDelayMs = 200L
 
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
